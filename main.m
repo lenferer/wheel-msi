82e2981e8f164dff4f18a86dd571a172534b8b27
Sergey Kharchenko <ss.kharchenko@gmail.com>
2019-08-06 18:42:34 +0700

    add comments

--- main.m
+++ main.m
@@ -2,18 +2,26 @@ clc;
 clear all;
 S=[];
 k=0;
-X=dlmread('G:\DSP project\drive-download-20160921T174509Z\Mayank_ssvep\r9.txt');
+% M = dlmread(filename) reads an ASCII-delimited numeric data file into matrix M. The dlmread function detects the delimiter from the file and treats repeated white spaces as a single delimiter.
+X = dlmread('G:\DSP project\drive-download-20160921T174509Z\Mayank_ssvep\r9.txt');
+% [b, a] = butter (n, Wn, ftype) создает фильтр Баттерворта нижних, верхних, полосовых или полосовых помех в зависимости от значения ftype и количества элементов Wn. Результирующие полосовые и полосовые конструкции имеют порядок 2n.
+% Примечание. Информацию о численных проблемах, влияющих на формирование передаточной функции, см. В разделе «Ограничения».
 [b,a] = butter(4,[5/256 30/256],'bandpass');
+% y = filter (b, a, x) фильтрует входные данные x, используя рациональную передаточную функцию, определенную коэффициентами числителя и знаменателя b и a.
 X = filter(b,a,X);
+% X = zeros (sz1, ..., szN) возвращает массив нулей sz1 by -...- by-szN, где sz1, ..., szN указывают размер каждого измерения. Например, zeros (2,3) возвращают матрицу 2 на 3.
 z_act=zeros(140,1);
 i=1;
 z_act=[];
 % for i=1:140
+% X = ones (sz1, ..., szN) возвращает массив sz1 by -...- by-szN, где sz1, ..., szN указывает размер каждого измерения. Например, ones (2,3) возвращает массив единиц 2 на 3.
 z_req=[ones(20,1)*1;ones(20,1)*2;ones(20,1)*3;ones(20,1)*4;ones(20,1)*5;ones(20,1)*6;ones(20,1)*7];%
 % z_req=flipud(z_req);
 % z_req(i)=ceil(i/20);
 % end
 % z_req=[ones(60,1)*1];
+% y = buffer (x, n) делит вектор сигнала длины L на неперекрывающиеся сегменты данных (кадры) длины n. Каждый кадр данных занимает один столбец матричного вывода y, который имеет n строк и столбцов ceil (L / n). Если L не делится поровну на n, последний столбец дополняется нулями до длины n.
+% y = buffer (x, n, p) перекрывает или перекрывает последовательные кадры в выходной матрице на p выборок:
 out = buffer(X(:,18)',511,255);
 q = size(out);
 while(i<=q(2))
@@ -57,4 +65,5 @@ while(i<141)
     end
     i=i+1;
 end
+% С = confusionmat (group, grouphat) возвращает спутанность матрицу С, определяемой известных и предсказанных группами в group и grouphat, соответственно.
 [C,order]=confusionmat(z_req,z_act);
